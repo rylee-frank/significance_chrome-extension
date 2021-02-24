07214e7cb391f194cbd6d558e0fdae77c7fcaebc
rylee-frank <rfmackenzie25@gmail.com>
2021-02-23 21:49:26 -0700

    color changes, jquery format

--- js/content.js
+++ js/content.js
@@ -1,82 +1,80 @@
-
-/* Referenced joya:Jquery function to generate Random    font Size & Color 
-http://jsfiddle.net/joya/7ys9s5b1/*/
+//REFERENCE
+//joya:Jquery function to generate Random font Size & Color 
+//http://jsfiddle.net/joya/7ys9s5b1/
 
 function variables(){
+    function randomNumberGenerator(min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    /*               FONT SIZE               */
     
-    function randomNumberGenerator(min,max)
-{
-return Math.floor(Math.random()*(max-min+1)+min);
-}
+    var minFontSize = 10;
+    var maxFontSize = 32;
+        $('a').css("fontSize", randomNumberGenerator(minFontSize, maxFontSize));   
+        $('.eebAO').css("fontSize", randomNumberGenerator(minFontSize, maxFontSize)); 
+    var minFontSize2 = 14;
+    var maxFontSize2 = 48;
+        $('h3').css("fontSize", randomNumberGenerator(minFontSize2, maxFontSize2));
+        $('span').css("fontSize", randomNumberGenerator(minFontSize2, maxFontSize2));
+    var minFontSize3 = 18;
+    var maxFontSize3 = 28;
+        $('abbr').css("fontSize", randomNumberGenerator(minFontSize3, maxFontSize3));
+
+    /*               LETTER SPACING               */
     
-var minFontSize = 10;
-var maxFontSize = 32;
+    var minFontSpacing = -9;
+    var maxFontSpacing = 6;
 
-$('a').css("fontSize", randomNumberGenerator(minFontSize, maxFontSize));
+    $('a').css("letter-spacing", randomNumberGenerator(minFontSpacing, maxFontSpacing));
+    
+    var minFontSpacing2 = -3;
+    var maxFontSpacing2 = 10;
 
-var minFontSpacing = -9;
-var maxFontSpacing = 6;
+    $('span').css("letter-spacing", randomNumberGenerator(minFontSpacing2, maxFontSpacing2));
 
-$('a').css("letter-spacing", randomNumberGenerator(minFontSpacing, maxFontSpacing));
+//REFERENCE
+//html5andblog: Random Web Font From Array - JQuery
+//https://codepen.io/html5andblog/pen/mygJNw
 
-$('abbr').css("fontSize", randomNumberGenerator(minFontSize2, maxFontSize2));
+    /*               FONT COLOR              */    
+    
+    var letterColor = [
+        "#79C8C8",
+        "#B6D1D0",
+        "#8B9E9D",
+        "#D7FCFB",
+        "BLACK",
+        "WHITE",];
+    letterColor = letterColor[Math.floor(Math.random() * letterColor.length)];
 
-var minFontSize2 = 14;
-var maxFontSize2 = 48;
-
-$('h3').css("fontSize", randomNumberGenerator(minFontSize2, maxFontSize2));
-
-$('span').css("fontSize", randomNumberGenerator(minFontSize2, maxFontSize2));
-
-$('.eebAO').css("fontSize", randomNumberGenerator(minFontSize2, maxFontSize2));    
-var minFontSpacing2 = -3;
-var maxFontSpacing2 = 10;
-
-$('span').css("letter-spacing", randomNumberGenerator(minFontSpacing2, maxFontSpacing2));
-
- /*Reference html5andblog: Random Web Font From Array - JQuery
- https://codepen.io/html5andblog/pen/mygJNw*/
-
-var letterColor = [
-    "white",
-    "black",
-    "light-grey",
-    "pink",];
-letterColor = letterColor[Math.floor(Math.random() * letterColor.length)];
-
-$('a').css('color', letterColor);
+    $('a').css('color', letterColor);
+    $('.eebAO').css('color', letterColor);
 
 var letterColor2 = [
-    "white",
-    "black",
-    "light-grey",
-    "pink",];
+    "#79C8C8",
+    "#B6D1D0",
+    "#8B9E9D",
+    "#D7FCFB",
+    "BLACK",
+    "WHITE",];
+    
 letterColor2 = letterColor2[Math.floor(Math.random() * letterColor2.length)];
+    
 $('span').css('color', letterColor2);
 
 var letterColor3 = [
-    "white",
-    "black",
-    "light-grey",
-    "pink",];
+    "#79C8C8",
+    "#B6D1D0",
+    "#8B9E9D",
+    "#D7FCFB",
+    "BLACK",
+    "WHITE",];
 letterColor3 = letterColor3[Math.floor(Math.random() * letterColor3.length)];
 $('h3').css('color', letterColor3);
 }   
-//function init(){
-//    variables();
-//    var legibility = function(){
-//        var rand = Math.round(Math.random() * (10000 - 800)) + 500; // generate new time (between 3sec and 500"s)
-//        setInterval(legibility, rand);
-//    }
-//    legibility();
-//
-////var idVar = setInterval(() => {            
-//
-////}, 1500); 
-//}
-//$(function(){
-//    init();
-//});
+/*  REFERENCE
+Alexander Mills: Randomize setInterval
+https://stackoverflow.com/questions/6962658/randomize-setinterval-how-to-rewrite-same-random-after-random-interval*/
     var keepLooping = true;
     (function ontimeout(){
         if(keepLooping){
