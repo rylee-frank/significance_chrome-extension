12a8e56e009278eb56bc3d27bbea11fe5c274e48
rylee-frank <rfmackenzie25@gmail.com>
2021-02-23 19:54:04 -0700

    font uploads

--- js/content.js
+++ js/content.js
@@ -1,15 +1,77 @@
-var fontArray = [
-    "'Amatic SC', cursive",
-    "'Anton', sans-serif",
-    "'Libre Barcode 39 Extended', cursive"];
-selectFont = fontArray[Math.floor(Math.random() * fontArray.length)];
-$('.sqdOP.yWX7d._8A5w5.ZIAjV').css('font-family', selectFont);
-$('.sqdOP.yWX7d._8A5w5.ZIAjV').css('letter-spacing', '-3px');
+            /*      INSTAGRAM       */
+var fontArrayI = [
+    "'Libre Barcode 39 Extended', normal",
+    "'Amatic SC', normal",
+    "Inconsolata, normal",
+    "'Raleway', normal",
+    "'Sigmar One', noraml"];
+var letterSpacingI = [
+    "-3",
+    "-1",
+    "-6",
+    "4",];
+selectFontI = fontArrayI[Math.floor(Math.random() * fontArrayI.length)];
+legibilityI = letterSpacingI[Math.floor(Math.random()*letterSpacingI.length)];
+$('.Ppjfr.UE9AK.wdOqh').css('font-family', selectFontI);
+$('.Ppjfr.UE9AK.wdOqh').css('letter-spacing', legibilityI);
 
-var fontArray2 = [
-    "'Amatic SC', cursive",
-    "'Anton', sans-serif",
-    "'Libre Barcode 39 Extended', cursive"];
-selectFont2 = fontArray2[Math.floor(Math.random() * fontArray2.length)];
-$('.eo2As').css('font-family', selectFont2);
-$('.eo2As').css('letter-spacing', '-5px');
+var fontArrayI2 = [
+    "'Libre Barcode 39 Extended', normal",
+    "'Amatic SC', normal",
+    "Inconsolata, normal",
+    "'Raleway', normal",
+    "'Sigmar One', noraml"];
+var letterSpacingI2 = [
+    "-3",
+    "-1",
+    "-6",
+    "4",];
+selectFontI2 = fontArrayI2[Math.floor(Math.random() * fontArrayI2.length)];
+legibilityI2 = 
+letterSpacingI2[Math.floor(Math.random()*letterSpacingI.length)];
+$('.eo2As').css('font-family', selectFontI2);
+$('.eo2As').css('letter-spacing', legibilityI2);
+
+
+            /*      FACEBOOK       */
+var fontArrayF = [
+    "'Libre Barcode 39 Extended', normal",
+    "'Amatic SC', normal",
+    "Inconsolata, normal",
+    "'Raleway', normal",
+    "'Sigmar One', noraml"];
+var letterSpacingF = [
+    "-3",
+    "-1",
+    "-6",
+    "4",];
+selectFontF = fontArrayF[Math.floor(Math.random() * fontArrayF.length)];
+legibilityF = letterSpacingF[Math.floor(Math.random()*letterSpacingF.length)];
+$('h3').css('font-family', selectFontF);
+$('h3').css('letter-spacing', legibilityF);
+
+var fontArrayF2 = [
+    "'Libre Barcode 39 Extended', normal",
+    "'Amatic SC', normal",
+    "Inconsolata, normal",
+    "'Raleway', normal",
+    "'Sigmar One', noraml"];
+var letterSpacingF2 = [
+    "-3",
+    "-1",
+    "-6",
+    "4",];
+selectFontF2 = fontArrayF2[Math.floor(Math.random() * fontArrayF2.length)];
+legibilityF2= letterSpacingF2[Math.floor(Math.random()*letterSpacingF2.length)];
+$('._4g34._5i2i._52we').css('font-family', selectFontF2);
+$('._4g34._5i2i._52we').css('letter-spacing', legibilityF2);
+
+//var minFontSize = 8;
+//var maxFontSize = 24;
+//
+//$('.Ppjfr.UE9AK.wdOqh').css("fontSize", randomNumberGenerator(minFontSize, maxFontSize));
+//
+//function randomNumberGenerator(min,max)
+//{
+//return Math.floor(Math.random()*(max-min+1)+min);
+//}
